ce7371169a66d0c2f8b2386b50f551b21ce1d64e
小卓子 <idozhuoyong@qq.com>
2021-12-06 14:49:39 +0800

    03.py

--- 01-Python/菜鸟 Python 100 例/03.py
+++ 01-Python/菜鸟 Python 100 例/03.py
@@ -11,3 +11,15 @@
 #         print(num)
 #         break
 #     num += 1
+
+"""
+1. x+100 = n*n; x+100+168=m*m;
+2. m*m - n*n = 168 ==> (m+n)(m-n)=168;
+3. m+n=i; m-n+j; ==> i*j=168; n=(i-j)/2; m=(i+j)/2;
+"""
+for i in range(1, 85):
+    if 168 % i == 0:
+        j = 168 // i
+        if i > j and (i-j)%2 == 0 and (i+j)%2 == 0:
+            x = ((i-j)//2) * ((i-j)//2) - 100
+            print(x)
