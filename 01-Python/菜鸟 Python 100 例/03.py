90e682c571c8b5c998a20dbcb62926a53206ca0d
小卓子 <idozhuoyong@qq.com>
2021-12-06 11:52:18 +0800

    03.py

---
+++ 01-Python/菜鸟 Python 100 例/03.py
@@ -0,0 +1,13 @@
+"""
+题目：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
+"""
+
+# import math
+# num = 0
+# while True:
+#     a1 = math.sqrt(num + 100)
+#     a2 = math.sqrt(num + 100 + 168)
+#     if (a1/1 == a1//1) and (a2/1 == a2//1):
+#         print(num)
+#         break
+#     num += 1
