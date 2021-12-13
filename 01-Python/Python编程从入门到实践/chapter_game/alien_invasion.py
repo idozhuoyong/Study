0d3f28766eb4ac2178dfea66d75fec45d961e3bd
小卓子 <idozhuoyong@qq.com>
2021-12-13 16:52:04 +0800

    settings

--- 01-Python/Python编程从入门到实践/chapter_game/alien_invasion.py
+++ 01-Python/Python编程从入门到实践/chapter_game/alien_invasion.py
@@ -1,19 +1,22 @@
 import sys
 import pygame
 
+from settings import Settings
+
 def run_game():
     # 初始化
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800)) # 创建窗口
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) # 创建窗口
     pygame.display.set_caption("Alien Invasion") # 设置说明
-
-    screen.fill((245, 245, 245)) # 填充背景颜色
 
     while True:
         # 监听键盘和鼠标
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(ai_settings.bg_color) # 填充背景颜色
         
         # 让最近绘制的屏幕可见
         pygame.display.flip()
---
+++ 01-Python/Python编程从入门到实践/chapter_game/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """ 存储所有设置的类 """
+    def __init__(self):
+        """ 初始化设置 """
+        self.screen_width = 375
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+    
