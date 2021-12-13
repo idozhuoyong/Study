52695846cf46ac2c3f2b25b5bc891e233929a810
小卓子 <idozhuoyong@qq.com>
2021-12-13 16:21:02 +0800

    game

--- 01-Python/Python编程从入门到实践/chapter_game/alien_invasion.py
+++ 01-Python/Python编程从入门到实践/chapter_game/alien_invasion.py
@@ -1,8 +1,21 @@
 import sys
 import pygame
 
-def run_gane():
+def run_game():
     # 初始化
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien invasion")
+    screen = pygame.display.set_mode((1200, 800)) # 创建窗口
+    pygame.display.set_caption("Alien Invasion") # 设置说明
+
+    screen.fill((245, 245, 245)) # 填充背景颜色
+
+    while True:
+        # 监听键盘和鼠标
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
