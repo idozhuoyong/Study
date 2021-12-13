50a785930cae5425d556b47404fbfe6efba5c28a
小卓子 <idozhuoyong@qq.com>
2021-12-13 17:11:19 +0800

    ship

--- 01-Python/Python编程从入门到实践/chapter_game/alien_invasion.py
+++ 01-Python/Python编程从入门到实践/chapter_game/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # 初始化
@@ -10,13 +11,18 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) # 创建窗口
     pygame.display.set_caption("Alien Invasion") # 设置说明
 
+    # 创建一艘飞船
+    ship = Ship(screen)
+
     while True:
         # 监听键盘和鼠标
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        # 重新绘制屏幕
         screen.fill(ai_settings.bg_color) # 填充背景颜色
+        ship.blitme()
         
         # 让最近绘制的屏幕可见
         pygame.display.flip()
--- 01-Python/Python编程从入门到实践/chapter_game/settings.py
+++ 01-Python/Python编程从入门到实践/chapter_game/settings.py
@@ -3,6 +3,6 @@ class Settings():
     def __init__(self):
         """ 初始化设置 """
         self.screen_width = 375
-        self.screen_height = 800
+        self.screen_height = 750
         self.bg_color = (230, 230, 230)
     
