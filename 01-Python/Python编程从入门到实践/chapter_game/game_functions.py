ecfd24a3f1e2eede89f9deed83a56023da1e0932
小卓子 <idozhuoyong@qq.com>
2021-12-15 16:44:29 +0800

    game

--- 01-Python/Python编程从入门到实践/chapter_game/game_functions.py
+++ 01-Python/Python编程从入门到实践/chapter_game/game_functions.py
@@ -1,25 +1,45 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """ 响应按键 """
+    if event.key == pygame.K_RIGHT:
+        # 向右移动
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        # 向左移动
+        ship.moving_left = True
+    elif event.key == pygame.K_TOP:
+        # 向上移动
+        ship.moving_top = True
+    elif event.key == pygame.K_BOTTOM:
+        # 向下移动
+        ship.moving_bottom = True
+
+def check_keyup_events(event, ship):
+    """ 响应松开 """
+    if event.key == pygame.K_RIGHT:
+        # 结束向右移动
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        # 结束向左移动
+        ship.moving_left = False
+    elif event.key == pygame.K_TOP:
+        # 结束向上移动
+        ship.moving_top = False
+    elif event.key == pygame.K_BOTTOM:
+        # 结束向下移动
+        ship.moving_bottom = False
+
 def check_event(ship):
     """ 响应按键和鼠标时间 """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # 向右移动
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                # 向左移动
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                # 结束向右移动
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                # 结束向左移动
-                ship.moving_left = False
+            check_keyup_events(event, ship)
     
 def update_screen(ai_settings, screen, ship):
     """ 更新屏幕上的图像，并切换到新图像 """
--- 01-Python/Python编程从入门到实践/chapter_game/ship.py
+++ 01-Python/Python编程从入门到实践/chapter_game/ship.py
@@ -17,20 +17,28 @@ class Ship:
         self.rect.bottom = self.screen_rect.bottom
 
         # 在飞船的属性center中存储小数值
-        self.center = float(self.rect.centerx)
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
 
         # 移动标志
         self.moving_right = False
         self.moving_left = False
+        self.moving_top = False
+        self.moving_bottom = False
     
     def update(self):
-        if self.moving_right:
-            self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
-            self.center -= self.ai_settings.ship_speed_factor
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.centerx += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.centerx -= self.ai_settings.ship_speed_factor
+        if self.moving_top and self.rect.top < self.screen_rect.top:
+            self.centery += self.ai_settings.ship_speed_factor
+        if self.moving_bottom and self.rect.bottom > self.screen_rect.bottom:
+            self.centery -= self.ai_settings.ship_speed_factor
         
         # 根据 self.center 更新 rect 对象
-        self.rect.centerx = self.center
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
     
     def blitme(self):
         """ 在制定位置绘制飞船 """
