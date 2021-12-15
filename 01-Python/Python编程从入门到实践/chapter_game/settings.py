337b1fb9a710a6d04272a12c7d569e5e0a292cab
小卓子 <idozhuoyong@qq.com>
2021-12-15 17:37:21 +0800

    bullet

--- 01-Python/Python编程从入门到实践/chapter_game/settings.py
+++ 01-Python/Python编程从入门到实践/chapter_game/settings.py
@@ -6,6 +6,11 @@ class Settings():
         self.screen_height = 750
         self.bg_color = (230, 230, 230)
 
-        # 飞船的设置
-        self.ship_speed_factor = 1.5
-    
+        # 飞船设置
+        self.ship_speed_factor = 1.5 # 飞行速度
+
+        # 子弹设置
+        self.bullet_speed_factor = 1 # 子弹飞行速度
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
