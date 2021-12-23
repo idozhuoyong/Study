f6cc0e5fc563450d895d6e55e9055776695d4f28
小卓子 <idozhuoyong@qq.com>
2021-12-23 11:59:29 +0800

    game

--- 01-Python/Python编程从入门到实践/chapter_game/game_functions.py
+++ 01-Python/Python编程从入门到实践/chapter_game/game_functions.py
@@ -56,6 +56,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """ 在玩家单击 Play 按钮时开始新游戏 """
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # 重置游戏设置
+        ai_settings.initialize_dynamic_settings()
         # 隐藏光标
         pygame.mouse.set_visible(False)
         # 重置游戏统计信息
@@ -94,6 +96,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     if len(aliens) == 0:
         # 删除现有的子弹并新建一群外星人
         bullets.empty()
+        # 提高游戏速速
+        ai_settings.increase_speed()
+        # 重新绘制外星人
         create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
