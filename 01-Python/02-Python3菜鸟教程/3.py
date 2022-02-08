4fbd7b0e8c66d36962fe373aa814ba56038dbe38
小卓子 <idozhuoyong@qq.com>
2022-02-08 16:21:36 +0800

    3.py

---
+++ 01-Python/02-Python3菜鸟教程/3.py
@@ -0,0 +1,68 @@
+# 算术运算符
+# a = 21
+# b = 10
+# c = 0
+
+# c = a + b
+# print("1 - c 的值为: ", c) # 31
+
+# c = a - b
+# print("2 - c 的值为: ", c) # 11
+
+# c = a * b
+# print("3 - c 的值为: ", c) # 210
+
+# c = a / b
+# print("4 - c 的值为: ", c) # 2.1
+
+# c = a % b
+# print("5 - c 的值为: ", c) # 1
+
+# # 修改变量 a、b、c
+# a = 2
+# b = 3
+# c = a ** b
+# print("6 - c 的值为: ", c) # 8
+
+# a = 10
+# b = 5
+# c = a // b
+# print("7 - c 的值为: ", c) # 2
+
+
+# 比较运算符
+a = 21
+b = 10
+c = 0
+
+if a == b :
+    print("1 - a 等于 b")
+else:
+    print("1 - a 不等于 b") # 输出
+
+if a != b :
+    print("2 - a 不等于 b") # 输出
+else:
+    print("2 - a 等于 b") 
+
+if a < b :
+    print("3 - a 小于 b") 
+else:
+    print("3 - a 大于等于 b") # 输出 
+
+if a > b :
+    print("4 - a 大于 b") # 输出 
+else:
+    print("4 - a 小于等于 b") 
+
+a = 5
+b = 20
+if a <= b :
+    print("5 - a 小于等于 b") # 输出
+else:
+    print("5 - a 大于 b")
+
+if b >= a:
+    print("6 - b 大于等于 a") # 输出
+else:
+    print("6 - b 小于 a")
