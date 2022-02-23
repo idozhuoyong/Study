8932f287484d3c67955f8d010ab847eba0046476
小卓子 <idozhuoyong@qq.com>
2022-02-23 16:56:29 +0800

    5.py

---
+++ 2-Python/1-Python3菜鸟教程/5.py
@@ -0,0 +1,19 @@
+'''
+斐波拉契数列
+'''
+# a, b = 0, 1
+# while b < 1000:
+#     print(b, end=', ')
+#     a, b = b, a+b
+# print()
+
+# for i in range(5, 9):
+#     print(i)
+
+
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+print(max(10, 80))
