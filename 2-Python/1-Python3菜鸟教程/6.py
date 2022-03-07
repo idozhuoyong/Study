f6ec1db84aea754e6db49c8a52a569d4d8c98818
小卓子 <idozhuoyong@qq.com>
2022-03-07 09:51:54 +0800

    xx

--- 2-Python/1-Python3菜鸟教程/6.py
+++ 2-Python/1-Python3菜鸟教程/6.py
@@ -1,3 +1,23 @@
-f = open('./tmp/foo.txt', "w")
-f.write("我是一门语言")
+# f = open('./tmp/foo.txt', "w")
+# f.write("Python 是一个非常好的语言。\n是的，的确非常好！！\n")
+# f.close()
+
+# f = open("./tmp/foo.txt", "r")
+# str = f.read()
+# print(str)
+# f.cloese()
+
+# f = open("./tmp/foo.txt", "r")
+# str = f.readline()
+# print(str)
+# f.close()
+
+# f = open("./tmp/foo.txt", "r")
+# str = f.readlines()
+# print(str)
+# f.close()
+
+f = open("./tmp/foo.txt", "r")
+for line in f:
+    print(line, end="")
 f.close()
