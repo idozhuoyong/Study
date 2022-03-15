6e7f23f5dab2abb57ae2f2c1e10353fa2789e6f0
小卓子 <idozhuoyong@qq.com>
2022-03-15 16:24:48 +0800

    xx

--- 2-Python/1-Python3菜鸟教程/6.py
+++ 2-Python/1-Python3菜鸟教程/6.py
@@ -17,7 +17,34 @@
 # print(str)
 # f.close()
 
-f = open("./tmp/foo.txt", "r")
-for line in f:
-    print(line, end="")
-f.close()
+# f = open("./tmp/foo.txt", "r")
+# for line in f:
+#     print(line, end="")
+# f.close()
+
+# while True:
+#     try:
+#         num = int(input("请输入一个数字："))
+#         print(num)
+#         break
+#     except ValueError:
+#         print("您输入的不是数字")
+
+# x = 10
+# if x > 5:
+#     raise Exception('x 不能大于 5。x 的值为：{}'.format(x))
+
+# class MyError(Exception):
+#     def __init__(self, value) -> None:
+#         super().__init__()
+#         self.value = value
+    
+#     def __str__(self) -> str:
+#         return repr(self.value)
+
+# try:
+#     raise MyError(2*2)
+# except MyError as e:
+#     print('My exception occurred, value: ', e.value)
+
+# raise MyError('oops!')
