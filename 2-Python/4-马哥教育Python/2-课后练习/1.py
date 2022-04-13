54b472ff512c2d426e5b7417c94cfe34371d1907
卓勇 <idozhuoyong@qq.com>
2022-04-13 23:14:12 +0800

    九九乘法表

--- 2-Python/4-马哥教育Python/2-课后练习/1.py
+++ 2-Python/4-马哥教育Python/2-课后练习/1.py
@@ -1,6 +1,12 @@
 # 九九乘法表
 
+# 方式一
+# for i in range(1, 10):
+#     for j in range(1, i+1):
+#         print("{}*{}={:<2}".format(j, i, i*j), end=" ")
+#     print()
+
+# 方式二
 for i in range(1, 10):
     for j in range(1, i+1):
-        print("{}*{}={:<2d}".format(j, i, i*j), end=" ")
-    print()
+        print("{}*{}={:<2}".format(j, i, i*j), end='\n' if i == j else ' ')
