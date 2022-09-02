22d5752de160839f971467f1ab21a73c93bc37bd
小卓子 <idozhuoyong@qq.com>
2022-09-02 10:52:46 +0800

    swiftui

--- 3-Swift/2-SwiftUI/Landmarks/Landmarks/Views/MapView.swift
+++ 3-Swift/2-SwiftUI/Landmarks/Landmarks/Views/MapView.swift
@@ -17,8 +17,11 @@ struct MapView: UIViewRepresentable {
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         //let coordinate = CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868)
+        // 设置经纬度跨度，越小越详细
         let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        // 设置经纬度区域
         let region = MKCoordinateRegion(center: coordinate, span: span)
+        // 设置地图显示区域
         uiView.setRegion(region, animated: true)
     }
 }
