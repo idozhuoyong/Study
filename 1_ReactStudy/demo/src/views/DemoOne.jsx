9c587ac5a42aab2fdb786c22dc1d7c5cf07e2f4d
卓勇 <idozhuoyong@qq.com>
2025-05-18 23:16:05 +0800

    10.关于 props 属性的细节知识

--- 1_ReactStudy/demo/src/views/DemoOne.jsx
+++
@@ -1,9 +0,0 @@
-const DemoOne = function DemoOne(props) {
-    console.log(props);
-    
-    return <div className="demo-box">
-        我是 DEMO-ONE
-    </div>;
-}
-
-export default DemoOne
---
+++ 1_ReactStudy/demo/src/views/DemoOne1.jsx
@@ -0,0 +1,24 @@
+const DemoOne = function DemoOne(props) {
+    let { className, style, title } = props
+    let { x = 0 } = props
+    console.log(title, x)
+    
+    return <div className={`demo-box ${className}`} style={style}>
+        <h2 className="title">{title}</h2>
+    </div>;
+}
+
+// 通过把函数当做对象，设置静态的私有属性方法，来给其设置属性的校验规则
+// 「15.5之后不推荐，react19已废弃」
+// DemoOne.defaultProps = {
+//     x: 0
+// };
+
+// 参数校验
+// 「15.5之后不推荐，react19已废弃」
+// DemoOne.propTypes = {
+//     title: PropTypes.string.isRequired,
+//     x: PropTypes.number
+// }
+
+export default DemoOne;
