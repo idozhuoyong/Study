c34c776c86dae2c42199da0ff0e0a5785b3ecb2a
卓勇 <idozhuoyong@qq.com>
2025-08-17 23:01:26 +0800

    36.useEffect的基础知识和底层机制

---
+++ 1_ReactStudy/备份/36.useEffect的基础知识和底层机制/demo2/src/views/Demo3.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Button } from 'antd';
+import './Demo.less';
+import { flushSync } from "react-dom";
+        
+const Demo = function Demo() {
+    console.log("RENDER渲染");
+    
+    let [x, setX] = useState(10);
+    let [y, setY] = useState(20);
+    let [z, setZ] = useState(30);
+
+    let handleAddClick = () => {
+        // useState 是异步处理的（执行三次，只渲染一次）
+        setX(x + 1);
+        flushSync(); // 变为同步处理
+        setY(y + 1);
+        setZ(z + 1);
+    };
+
+    return <div className="demo">
+        <span className="num">x:{x}</span>
+        <span className="num">y:{y}</span>
+        <span className="num">z:{z}</span>
+        <Button
+            type="primary"
+            size="small"
+            onClick={handleAddClick}>
+            新增
+        </Button>
+    </div>;
+};
+
+export default Demo;
