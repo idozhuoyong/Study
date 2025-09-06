79a3bd26c712727ea376328518062384d611ca04
卓勇 <idozhuoyong@qq.com>
2025-09-06 23:13:35 +0800

    43. 基于自定义 Hook 提取公共逻辑

--- 1_ReactStudy/demo3/src/index.jsx
+++ 1_ReactStudy/demo3/src/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 // import Demo from './views/Demo1';
-import Demo from './views/Demo2';
+// import Demo from './views/Demo2';
+import Demo from './views/Demo3';
 /* 使用ANTD组件库 */
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
---
+++ 1_ReactStudy/demo3/src/views/Demo3.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Button } from "antd";
+import './Demo.less'
+
+/*
+自定义Hook
+    作用：提取封装一些公共的处理逻辑
+    玩法：创建一个函数，名字需要是useXxx，后期就可以在组件中调用这个方法
+*/
+
+const usePartialState = function usePartialState(initialValue) { 
+    let [state, setState] = useState(initialValue);
+    // setState: 不支持部分状态更改
+    // setPartial: 我们期望可以支持部分状态的更改
+    const setPartial = function setPartial(partialState) {
+        setState({
+            ...state,
+            ...partialState
+        })
+    }
+
+
+    return [state, setPartial];
+};
+
+const Demo = function Demo() {
+    let [state, setPartial] = usePartialState({
+        supNum: 10,
+        oppNum: 5
+    })
+    const handle = (type) => {
+        if (type === 'sup') {
+            setPartial({
+                supNum: state.supNum + 1
+            })
+            return;
+        }
+        setPartial({
+            oppNum: state.oppNum + 1
+        })
+    }
+
+    return <div className="vote-box">
+        <div className="main">
+            <p>支持人数：{state.supNum}人</p>
+            <p>反对人数：{state.oppNum}人</p>
+        </div>
+        <div className="footer">
+            <Button type="primary" onClick={handle.bind(null, 'sup')}>支持</Button>
+            <Button type="primary" danger onClick={handle.bind(null, 'opp')}>反对</Button>
+        </div>
+    </div>
+}
+
+export default Demo;
